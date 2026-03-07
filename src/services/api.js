bda0247aaf9400c1bc1c2beeae69e96b1e862028
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-03-07 11:52:55 +0800

    Fix port for Render

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // If you don't have your Render URL yet, use this placeholder 
   // so the build passes. Update it later!
-  baseURL: "https://backend-1-k3zu.onrender.com" 
+  baseURL: "https://backend-1-k3zu.onrender.com/api/moods" 
 });
 
 // THIS IS THE LINE YOU ARE MISSING
