d7936ca8adb543cae66fcd8a73477ba79c9cb415
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-03-07 12:06:03 +0800

    sad

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // If you don't have your Render URL yet, use this placeholder 
-  // so the build passes. Update it later!
-  baseURL: "https://backend-1-k3zu.onrender.com/api/moods" 
+  // Tanggalin ang /api sa dulo para hindi mag-doble
+  baseURL: "https://backend-1-k3zu.onrender.com" 
 });
 
-// THIS IS THE LINE YOU ARE MISSING
 export default api;
