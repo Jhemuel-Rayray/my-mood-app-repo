edff22bb53fd8d2566eaf8a97241d54c53ea894f
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-03-07 12:11:32 +0800

    final

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+// Sa halip na api.get("/moods"), dapat ay:
+await api.get("/api/moods");
+await api.post("/api/moods", data);
 
 const api = axios.create({
-  // Tanggalin ang /api sa dulo para hindi mag-doble
+  // Tanggalin ang /api sa dulo
   baseURL: "https://backend-1-k3zu.onrender.com" 
 });
 
