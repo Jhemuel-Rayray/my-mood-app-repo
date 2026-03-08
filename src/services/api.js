7cb4610bd45f9ec39d2fd17ea157009f15a4fd74
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-03-08 17:42:19 +0800

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
+// src/services/api.js
 const api = axios.create({
-  
-  baseURL: "https://backend-1-k3zu.onrender.com" 
+  baseURL: 'https://backend-1-k3zu.onrender.com/api' // Added /api here
 });
 
 export default api;
